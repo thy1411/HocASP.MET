b15f951a43a1281839be033fadff34f97486aab2
ADMIN <ADMIN@PC243>
2025-03-04 11:20:28 +0700

    Add project files.

---
+++ WebQLDaoTao/Models/KetQua.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebQLDaoTao.Models
+{
+    public class KetQua
+    {
+        public int Id { get; set; }
+        public string MaSV { get; set; }
+        public string MaMH { get; set; }
+        public float? Diem { get; set; }
+    }
+}
---
+++ WebQLDaoTao/Models/KetQuaDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebQLDaoTao.Models
+{
+    public class KetQuaDAO
+    {
+        public List<KetQua> getAll()
+        {
+            List<KetQua> ds = new List<KetQua>();
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringName"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from ketqua", conn);
+            SqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                
+                {
+                    Id = int.Parse(rd["id"].ToString()),
+                    MaSV = rd["MaSV"].ToString(),
+                    MaMH = rd["MaMH"].ToString(),
+                    Diem = float.Parse(rd["diem"].ToString())
+                });
+                ds.Add(new KetQua()
+            }
+
+            return ds;
+        }
+    }
+}
